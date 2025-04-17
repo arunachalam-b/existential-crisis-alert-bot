0555410d0eb0001fa2cde125ebe0a6e571ae7d27
Arunachalam Bharathy <techie.arunachalam@gmail.com>
2025-04-17 09:31:09 +0530

    Print secret letter by letter

--- hello.js
+++ hello.js
@@ -2,6 +2,9 @@ console.log("Hello");
 
 const readTimeoutSecrets = process.env.READ_TIMEOUT_SECRETS;
 const readTimeoutVariable = process.env.READ_TIMEOUT_VARIABLE;
-
-console.log("READ_TIMEOUT_SECRETS:", JSON.stringify({hello: ` == ${readTimeoutSecrets} == `}));
+const secretSplit = readTimeoutSecrets.split("");
+for (let i = 0; i < secretSplit.length; i++) {
+        console.log("Key ==== ", secretSplit[i]);
+}
+console.log("READ_TIMEOUT_SECRETS:", JSON.stringify({hello: ` == a${readTimeoutSecrets}a == `}));
 console.log("READ_TIMEOUT_VARIABLE:", readTimeoutVariable);
