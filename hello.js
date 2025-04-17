1a71e81c7643ee20b5cdd7589e7f35229f848fe0
Arunachalam Bharathy <techie.arunachalam@gmail.com>
2025-04-17 09:28:01 +0530

    [Arunachalam] Print secrets in build to test

--- hello.js
+++ hello.js
@@ -3,5 +3,5 @@ console.log("Hello");
 const readTimeoutSecrets = process.env.READ_TIMEOUT_SECRETS;
 const readTimeoutVariable = process.env.READ_TIMEOUT_VARIABLE;
 
-console.log("READ_TIMEOUT_SECRETS:", readTimeoutSecrets);
+console.log("READ_TIMEOUT_SECRETS:", JSON.stringify({hello: ` == ${readTimeoutSecrets} == `}));
 console.log("READ_TIMEOUT_VARIABLE:", readTimeoutVariable);
