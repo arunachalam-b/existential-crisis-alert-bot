80f3fe7228f85d33485636b22133d0c1bfca5262
Arunachalam Bharathy <techie.arunachalam@gmail.com>
2025-04-17 16:58:26 +0530

    [Arunachalam] Print all envs

--- hello.js
+++ hello.js
@@ -8,6 +8,8 @@ const envs = [
 	"TWITTER_ACCESS_TOKEN_SECRET",
 ];
 
+console.log("All Envs ==== ", process.env);
+
 for (const env of envs) {
 	console.log("Environment Variable: ", env);
 	const secret = process.env[env];
