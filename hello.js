f3f8a92fa8d9f333c935fe00066f806a6813f4ae
Arunachalam Bharathy <techie.arunachalam@gmail.com>
2025-04-17 09:22:06 +0530

    [Arunachalam] Read secrets from environment on build

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
 console.log("Hello");
+
+const readTimeoutSecrets = process.env.READ_TIMEOUT_SECRETS;
+const readTimeoutVariable = process.env.READ_TIMEOUT_VARIABLE;
+
+console.log("READ_TIMEOUT_SECRETS:", readTimeoutSecrets);
+console.log("READ_TIMEOUT_VARIABLE:", readTimeoutVariable);
